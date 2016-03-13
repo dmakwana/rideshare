f59035e28b2725ef6dc90fbee0675bfc7cbc318b
a6haji <aly.haji99@gmail.com>
2016-03-13 17:43:00 -0400

    Facebook Button

---
+++ rideshare/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  rideshare
+//
+//  Created by Aly Haji on 2016-03-13.
+//  Copyright © 2016 Digvijay Makwana. All rights reserved.
+//
+
+#ifndef rideshare_Bridging_Header_h
+#define rideshare_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Bridging_Header_h */
