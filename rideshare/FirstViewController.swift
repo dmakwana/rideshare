e13e49c0e3ef3aa0bdbc470fe31b360c79c52cd5
Digvijay Makwana <d2makwan@uwaterloo.ca>
2016-03-25 13:04:40 -0400

    FB login button shows even if user already logged in

--- rideshare/FirstViewController.swift
+++ rideshare/FirstViewController.swift
@@ -16,16 +16,14 @@ class FirstViewController: UIViewController, FBSDKLoginButtonDelegate {
         
         if (FBSDKAccessToken.currentAccessToken() != nil)
         {
+            print("User is already logged in.");
             // User is already logged in, do work such as go to next view controller.
         }
-        else
-        {
-            let loginView : FBSDKLoginButton = FBSDKLoginButton()
-            self.view.addSubview(loginView)
-            loginView.center = self.view.center
-            loginView.readPermissions = ["public_profile", "email", "user_friends"]
-            loginView.delegate = self
-        }
+        let loginView : FBSDKLoginButton = FBSDKLoginButton()
+        self.view.addSubview(loginView)
+        loginView.center = self.view.center
+        loginView.readPermissions = ["public_profile", "email", "user_friends"]
+        loginView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
